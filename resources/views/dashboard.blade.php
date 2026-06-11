535008d82123e0a928575ba34e773d121a6aa028
Mauricio Alba <mauri.alba.soft@gmail.com>
2026-06-11 11:49:16 -0300

    Personalizacion de la vista del dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,37 @@
 <x-app-layout>
+
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-semibold text-xl">
+            Panel de Administración
         </h2>
     </x-slot>
 
     <div class="py-12">
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+
+            <div class="bg-white shadow rounded p-4">
+
+                <h3 class="mb-4">
+                    Administración del Portfolio
+                </h3>
+
+                <div class="d-grid gap-3">
+
+                    <a
+                        href="{{ route('experiencias.index') }}"
+                        class="btn btn-primary">
+
+                        Administrar Experiencias
+
+                    </a>
+
                 </div>
+
             </div>
+
         </div>
+
     </div>
+
 </x-app-layout>
